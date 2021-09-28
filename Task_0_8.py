7b673d50178b6032cf5844c2afb57ef7270f41e8
Mpho-L <mpholeqoalane@gmail.com>
2021-09-28 08:46:29 +0200

    updated Task_0_8

--- Task_0_8.py
+++ Task_0_8.py
@@ -1,9 +1,9 @@
 def number_to_time(number):
     hours = int(number / 60)
     minutes = number % 60
-    if hours > 1:
+    if hours > 1 or hours == 0:
         time = ( str(hours) + " hours "+ str(minutes) + " minutes")
     else: 
         time = ( str(hours) + " hour "+ str(minutes) + " minutes")
     return time
-print(number_to_time(71))
+print(number_to_time(80))
